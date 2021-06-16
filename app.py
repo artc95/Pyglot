f6a87c4e05c5a3b9b96bc207e1f865f94e63e9bf
Arthur Chionh (Hwai Teck) <arthur95chionh@gmail.com>
2021-06-16 13:03:09 +0800

    Fixed output table to show interactive answer review

--- app.py
+++ app.py
@@ -18,30 +18,31 @@ def pyglot():
     new_list = [] # store text with "the" replaced by questions
     response_list = [["Question","Review","Answer"]] # list to display table of question number, review of answer and answer buttons
     #answer_dict = {} # store dictionaries of each question's correct answer and answer review
-    the_count = 1 # keep track of number of "the"s in text
+    the_count = 0 # keep track of number of "the"s in text
     
     def review_answer(answer, question): # update answer review based on user's answer
         if answer == "the":
-            click_result[question].append("Question {} correct!".format(question))
+            click_result[question].reset("Question {} correct!".format(question+1))
 
         else:
-            click_result[question].append("Question {} wrong, try again!".format(question))
+            click_result[question].append("Question {} wrong, try again!".format(question+1))
 
     for word in raw_list:
         if word == "the":
-            new_list.append("{}. _________ ".format(the_count))
+            new_list.append("{}. _________ ".format(the_count+1))
             the_count += 1
         
         else:
             new_list.append(word)
     
     click_result = [output(put_text()) for i in range(the_count)]
+    print(click_result)
     for i in range(the_count):
-        response_list.append([str(i), click_result[i], put_buttons(["the", "-"], 
+        response_list.append([str(i+1), click_result[i], put_buttons(["the", "-"], 
             onclick=partial(review_answer, question=i))])
-    #=,[[str(i), click_result[i], put_buttons(["the", "-"], 
-            #onclick=partial(review_answer, question=i))] for i in range(the_count)]]
     
+    print(the_count)
+    print(response_list)
     new = " ".join(new_list) # combine list of questions and words, with " " as separator
 
     put_row(
